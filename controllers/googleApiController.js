b1c8619c43feca693cd10b0c5c018ab83991a84a
Victor Arabi <victorarabi@hotmail.com>
2022-12-17 00:56:19 -0500

    small adjustments

--- controllers/googleApiController.js
+++ controllers/googleApiController.js
@@ -81,7 +81,6 @@ async function getCalendarEvents(req, res, next) {
   const response = await calendar.events.list({
     calendarId: 'primary',
     timeMin: new Date().toISOString(),
-    maxResults: 10,
     singleEvents: true,
     orderBy: 'startTime',
   });
